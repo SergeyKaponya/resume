aa7f6a34871db0c73869617315462d39774a07ca
SergeyKaponya <128183533+SergeyKaponya@users.noreply.github.com>
2023-03-23 22:38:30 +0300

    Update analyze.php

--- analyze.php
+++ analyze.php
@@ -2,22 +2,22 @@
 // analyze.php
 header('Content-Type: text/html; charset=utf-8');
 
-require_once __DIR__ . '/vendor/autoload.php';
+$env = parse_ini_file(__DIR__ . '/.env');
+$api_key = isset($env['OPENAI_API_KEY']) ? $env['OPENAI_API_KEY'] : null;
 
-use Dotenv\Dotenv;
-
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+// Check if the API key is loaded
+if (!$api_key) {
+    die("OPENAI_API_KEY not found in .env file.");
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $resume_text = $_POST["resume"];
-    $prompt = "Analyze the following resume and give recommendations for improvement:\
-
+    $prompt = "Analyze the following resume and give recommendations for improvement:\n" . $resume_text . "\nRecommendations:";
 
     $url = "https://api.openai.com/v1/engines/davinci-codex/completions";
     $data = [
         "prompt" => $prompt,
-        "max_tokens" => 1000,
+        "max_tokens" => 100,
         "n" => 1,
         "stop" => null,
         "temperature" => 0.7,
@@ -25,7 +25,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $headers = [
         "Content-Type: application/json",
-        "Authorization: Bearer " . getenv("OPENAI_API_KEY"),
+        "Authorization: Bearer " . $api_key,
     ];
 
     $ch = curl_init();
@@ -39,6 +39,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $response = curl_exec($ch);
     $err = curl_error($ch);
+    $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE); // Get the HTTP status code
     curl_close($ch);
 
     if ($err) {
@@ -46,7 +47,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         error_log("cURL Error: " . $err); // Debug message
     } else {
         $json_response = json_decode($response, true);
-        $recommendations = $json_response["choices"][0]["text"];
-        echo nl2br(trim($recommendations));
+
+        // Check if the response has a "choices" key
+        if (isset($json_response["choices"])) {
+            $recommendations = $json_response["choices"][0]["text"];
+            echo nl2br(trim($recommendations));
+        } else {
+            echo "An error occurred. Please try again.";
+            error_log("OpenAI API Error: " . $response); // Debug message
+            error_log("HTTP Status Code: " . $http_status); // Debug message
+        }
     }
 }
